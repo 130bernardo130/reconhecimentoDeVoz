696bb50146093e580d3523248f59bd71d5af670d
130bernardo130 <bernardo.caco666@gmail.com>
2017-04-20 21:20:30 -0300

    Criação inicial do form
    o botão ja muda  texto

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace reconhecimentoDeVoz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ouvir = new System.Windows.Forms.Button();
+            this.texto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Ouvir
+            // 
+            this.Ouvir.Location = new System.Drawing.Point(76, 49);
+            this.Ouvir.Name = "Ouvir";
+            this.Ouvir.Size = new System.Drawing.Size(75, 29);
+            this.Ouvir.TabIndex = 0;
+            this.Ouvir.Text = "Ouvir";
+            this.Ouvir.UseVisualStyleBackColor = true;
+            this.Ouvir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // texto
+            // 
+            this.texto.Location = new System.Drawing.Point(12, 96);
+            this.texto.Name = "texto";
+            this.texto.Size = new System.Drawing.Size(202, 20);
+            this.texto.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 156);
+            this.Controls.Add(this.texto);
+            this.Controls.Add(this.Ouvir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ouvir;
+        private System.Windows.Forms.TextBox texto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reconhecimentoDeVoz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool botao = false;
+
+        private void mudaTexto(System.Windows.Forms.Button button,String texto)
+        {
+            Controls.Remove(button);
+            button.Text = texto;
+            Controls.Add(button);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            botao = !botao;
+           
+
+            if (botao)
+            {
+                mudaTexto(Ouvir, "Parar");
+            }
+            else
+            {
+                mudaTexto(Ouvir, "Ouvir");
+            }
+        }
+    }
+}
