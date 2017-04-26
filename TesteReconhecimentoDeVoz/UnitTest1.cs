aa1ed6919c7b620456a06b24f8de05ac4edbf93c
130bernardo130 <bernardo.caco666@gmail.com>
2017-04-25 21:15:01 -0300

    atualização da sala de aula
    nada tão importante para o código

---
+++ TesteReconhecimentoDeVoz/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using reconhecimentoDeVoz;
+
+namespace TesteReconhecimentoDeVoz {
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void TestFacebook() {
+            Voz voz = new Voz();
+            voz.ouvir();
+            System.Threading.Thread.Sleep(3000);
+            voz.parar();
+            Assert.AreEqual("facebook", voz.ultimoComando);
+        }
+
+
+        [TestMethod]
+        public void TestGoogle() {
+            Voz voz = new Voz();
+            voz.ouvir();
+            System.Threading.Thread.Sleep(3000);
+            voz.parar();
+            Assert.AreEqual("google", voz.ultimoComando);
+        }
+
+        [TestMethod]
+        public void Testavancar() {
+            Voz voz = new Voz();
+            voz.ouvir();
+            System.Threading.Thread.Sleep(3000);
+            voz.parar();
+            Assert.AreEqual("avançar", voz.ultimoComando);
+        }
+
+        [TestMethod]
+        public void TestVoltar() {
+            Voz voz = new Voz();
+            voz.ouvir();
+            System.Threading.Thread.Sleep(3000);
+            voz.parar();
+            Assert.AreEqual("voltar", voz.ultimoComando);
+        }
+        
+
+
+    }
+}
--- Voz.cs
+++ Voz.cs
@@ -4,42 +4,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Speech.Recognition;
+using System.Windows.Forms;
 
 namespace reconhecimentoDeVoz {
 
 
     public class Voz {
         private System.Windows.Forms.TextBox texto;
-        SpeechRecognitionEngine rec = null;
+        SpeechRecognitionEngine rec;
+        public string ultimoComando { get; set; }
 
-        public Voz(System.Windows.Forms.TextBox texto) {
-            this.texto = texto;
-            try
-            {
-                rec = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("pt-BR"));
+
+        public void configura() {
+            try {
+                this.rec = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("pt-BR"));
+            } catch (Exception) {
+                try {
+                    this.rec = new SpeechRecognitionEngine();
+                } catch (Exception) {
+                    MessageBox.Show("falhou ao fazer definição da engine de reconhecimento de voz");
+                }
+
+                MessageBox.Show("falha na definição de cultura");
             }
-            catch (Exception)
-            {
-                Console.WriteLine("falha na definição de cultura");
-                rec = new SpeechRecognitionEngine();
-            }
-            
+
             Choices comandos = new Choices();
-            String[] strComandos = new String[] { "pipa", "dois", "tres" };
+            String[] strComandos = new String[] { "Voltar", "pesquisar", "buscar", "avançar", "facebook", "google", "twitter", "instagram", "whatsapp", "favoritos", "apagar histórico", "youtube" };
             comandos.Add(strComandos);
             GrammarBuilder gCria = new GrammarBuilder();
             gCria.Append(comandos);
             Grammar gramatica = new Grammar(gCria);
             rec.LoadGrammarAsync(gramatica);
-            
+
             rec.SetInputToDefaultAudioDevice();
             rec.SpeechRecognized += Rec_SpeechRecognized;
+        }
+
+        public Voz() {
+            configura();
+        }
+
+        public Voz(System.Windows.Forms.TextBox texto) {
+            this.texto = texto;
+            configura();
         }
 
 
         private void Rec_SpeechRecognized(Object bj, SpeechRecognizedEventArgs e) {
             this.texto.Text = e.Result.Text;
-            Console.WriteLine(e.Result.Text);
+            ultimoComando = e.Result.Text;
+
+            switch (e.Result.Text) {
+                case "pesquisar":
+                case "buscar":
+                    Console.WriteLine("Navegador busca");
+                    break;
+                case "voltar":
+                    Console.WriteLine("Navegador está voltando para a página anterior");
+                    break;
+                case "avançar":
+                    Console.WriteLine("Navegador está indo para a próxima página");
+                    break;
+                case "facebook":
+                    Console.WriteLine("Navegador abre o facebook em uma nova aba");
+                    break;
+                case "google":
+                    Console.WriteLine("Navegador abre o google em uma nova aba");
+                    break;
+                case "twitter":
+                    Console.WriteLine("Navegador abre o twitter em uma nova aba");
+                    break;
+                case "instagram":
+                    Console.WriteLine("Navegador abre o instagram em uma nova aba");
+                    break;
+                case "whatsapp":
+                    Console.WriteLine("Navegador abre o facebook em uma nova aba");
+                    break;
+                case "favoritos":
+                    Console.WriteLine("Navegador abre os favoritos novas aba");
+                    break;
+                case "apagar histórico":
+                    Console.WriteLine("Navegador apaga o histórico");
+                    break;
+                case "youtube":
+                    Console.WriteLine("Navegador abre o youtube em uma nova aba");
+                    break;
+            }
+
+
         }
 
         public void ouvir() {
